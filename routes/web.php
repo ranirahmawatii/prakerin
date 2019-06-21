78329bbf117cc4fdcd495d3b9b8c7c12d0d40cb8
Rani <ranirahmawati0206@gmail.com>
2019-06-21 15:24:37 +0700

    tambahkan

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/archive', function () {
+    return view('frontend.archive');
+});
+Route::get('/category', function () {
+    return view('frontend.category');
+});
+Route::get('/contact', function () {
+    return view('frontend.contact');
+});
+Route::get('/elements', function () {
+    return view('frontend.elements');
+});
+Route::get('/new-details', function () {
+    return view('frontend.new-details');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin',function() {
+    return view('layouts.back');
+});
